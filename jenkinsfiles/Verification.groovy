24dd51d2920d66fd01433712ba717a3b8dbedf82
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-10-26 16:26:12 +0200

    Fixed solution name

--- jenkinsfiles/Verification.groovy
+++ jenkinsfiles/Verification.groovy
@@ -3,9 +3,9 @@ node {
 		checkout scm
 
 	stage 'Build'
-		bat '"C:/Program Files/Nuget/bin/nuget.exe" restore BalticMarinasBookWS.sln'
-		bat "\"${MSBuild}\" BalticMarinasBookWS.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+		bat '"C:/Program Files/Nuget/bin/nuget.exe" restore BalticMarinasBookMarinaWS.sln'
+		bat "\"${MSBuild}\" BalticMarinasBookMarinaWS.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
 	
 	stage 'Archive'
-		archive 'BalticMarinasBookWS/bin/Release/**'
+		archive 'BalticMarinasBookMarinaWS/bin/Release/**'
 }
