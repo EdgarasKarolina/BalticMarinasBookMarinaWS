cf41258ba6f2d70bd89ff19d907519a1b86bea1b
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-01 12:49:35 +0100

    Fixed again Verification pipeline

--- jenkinsfiles/Verification.groovy
+++ jenkinsfiles/Verification.groovy
@@ -3,7 +3,7 @@ node {
 		checkout scm
 
 	stage 'Build'
-		bat "\"${MSBuild}\" /t:Restore BalticMarinasBuySellWS.sln"
+		bat "\"${MSBuild}\" /t:Restore BalticMarinasBookMarinaWS.sln"
 		bat "\"${MSBuild}\" BalticMarinasBookMarinaWS.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
 	
 	stage 'Archive'
