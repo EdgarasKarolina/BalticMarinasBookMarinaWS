ea55b05fe32931607483e947e8440b7132ba363e
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-19 14:22:10 +0100

    Added necessary dependencies for Testing

--- BookMarina.Tests/Class1.cs
+++ BookMarina.Tests/Class1.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace BookMarina.Tests
 {
     public class Class1
     {
+        [Test]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = false;
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
     }
 }
