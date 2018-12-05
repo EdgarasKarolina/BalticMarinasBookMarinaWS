93ad253a522aa1d6d1d13cca7e573116de3f86ee
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-12-05 20:23:51 +0100

    Added email notification to jenkins jobs

--- jenkinsfiles/Testing.groovy
+++ jenkinsfiles/Testing.groovy
@@ -1,11 +1,57 @@
-node {
-	stage 'Checkout'
-		checkout scm
-
-	stage 'Test'
-		bat "\"${MSBuild}\" /t:Restore BalticMarinasBookMarinaWS.sln"
-		bat returnStatus: true, script: "\"C:/Program Files/dotnet/dotnet.exe\" test \"C:/Users/Edgaras/Desktop/Final Project/BalticMarinas/BalticMarinasBookMarinaWS/BalticMarinasBookMarinaWS.sln\" --logger \"trx;LogFileName=unit_tests.xml\" --no-build"
-	
-	stage 'Archive'
-		archive 'BalticMarinasBookMarinaWS/bin/Release/**'
-}
+pipeline
+	{
+	agent none
+		stages
+		{
+			stage('Checkout')
+			{
+				steps
+				{
+					script
+					{
+						node 
+						{
+							checkout scm
+						}
+					}
+				}
+			}
+			stage('Test')
+			{
+				steps
+				{
+					script
+					{
+						node 
+						{
+							bat "\"${MSBuild}\" /t:Restore BalticMarinasBookMarinaWS.sln"
+							bat returnStatus: true, script: "\"C:/Program Files/dotnet/dotnet.exe\" test \"C:/Users/Edgaras/Desktop/Final Project/BalticMarinas/BalticMarinasBookMarinaWS/BalticMarinasBookMarinaWS.sln\" --logger \"trx;LogFileName=unit_tests.xml\" --no-build"
+						}
+					}
+				}
+			}
+			stage('Archive')
+			{
+				steps
+				{
+					script
+					{
+						node 
+						{
+							archive 'BalticMarinasBookMarinaWS/bin/Release/**'
+						}
+					}
+				}
+			}
+	}
+		post
+		{
+			failure
+				{
+					mail to: 'attendancesystemkea@gmail.com',
+					subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+					body: "Something is wrong with ${env.BUILD_URL}"
+					echo "sent"
+				}
+	    }
+	}
